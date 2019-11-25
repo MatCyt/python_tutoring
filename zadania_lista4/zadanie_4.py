b16cfd07184d7895c3de0381c0256f7112ef2f1d
Cytrowski <Mateusz.Cytrowski@rb.com>
2019-11-25 12:02:15 +0100

    ex 4

--- zadania_lista4/zadanie_4.py
+++ zadania_lista4/zadanie_4.py
@@ -1,2 +1,39 @@
 # Napisz funkcję, która wypisze na ekranie n pierwszych wyrazów ciągu Fibonacciego.
+# Pierwszy wyraz jest równy 0, drugi jest równy 1, każdy następny jest sumą dwóch poprzednich.
 
+def fibonacci(ilosc_elementow):
+    if ilosc_elementow == 0:
+        return(0)
+    elif ilosc_elementow == 1:
+        return(1)
+    elif ilosc_elementow == 2:
+        return(1)
+    else:
+        return(fibonacci(ilosc_elementow-1) + fibonacci(ilosc_elementow-2))
+
+
+print(fibonacci(0))
+print(fibonacci(1))
+print(fibonacci(2))
+print(fibonacci(6))
+
+
+
+def fibonacci2(n): 
+    a = 0
+    b = 1
+    if n == 0: 
+        return a 
+    elif n == 1: 
+        return b 
+    else: 
+        for i in range(2,n): 
+            c = a + b 
+            a = b 
+            b = c 
+        return b 
+  
+print(fibonacci2(0))
+print(fibonacci2(1))
+print(fibonacci2(2))
+print(fibonacci2(6))
