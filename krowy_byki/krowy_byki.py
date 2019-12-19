41be457c0ec9fbf432b00af968992d04ff25fbc0
Cytrowski <Mateusz.Cytrowski@rb.com>
2019-12-19 10:01:55 +0100

    krowy i byki - pseudo kod

---
+++ krowy_byki/krowy_byki.py
@@ -0,0 +1,52 @@
+# KROWY I BYKI
+import random
+
+# FUNKCJA | generowanie losowej 4 cyfrowej liczby
+def get_random():
+    number = random.randint(1000, 9999)
+    return number
+
+# Input od użytkownika
+def get_input():
+    # pętla z while / if która sprawdza input i nie pozwala przejść dalej dopóki
+    # nie zostanie podany 4 cyfrowy int
+
+    # while True?
+
+    player_guess = int(input()) 
+    if len(player_guess) != 4:
+        print('alert')
+        continue
+    else:
+        break
+    
+
+# FUNKCJA | Input od użytkownika i jego walidacja (czy jest liczbą)
+
+# FUNKCJA | krowy i byki
+    # porównaj wylosowana i input
+    # dodaj krowę za poprawne miejsce i liczbę
+    # dodaj byka za poprawną liczbę
+    # zwróć odpowiedni format
+    # zlicz ilość prób
+    # pytaj dalej
+
+# funkcja | sprawdzenie czy kończyć grę
+
+# zakończ kiedy będzie odpowiednia ilość
+
+
+
+# main = main
+# liczba = FUN 1
+random_number = get_random()
+
+# stoper = 0
+play_game = 0
+
+while play_game = 0:
+    # FUN input - pętla z while żeby sprawdzać czy input jest zgodny?
+    # FUN krowy_byki
+    # FUN sprawdź czy koniec
+
+# zakończ
