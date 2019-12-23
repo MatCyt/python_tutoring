89aadf8993ba4205aed3b8c6672c69041d1a95e3
Cytrowski <Mateusz.Cytrowski@rb.com>
2019-12-23 16:07:11 +0100

    count_cows, count_buls, input validation

--- krowy_byki/krowy_byki.py
+++ krowy_byki/krowy_byki.py
@@ -5,35 +5,41 @@
 # 5555
 
 
-# KROWY I BYKI
+# COWS AND BULLS
 import random
 
-# FUNKCJA | generowanie losowej 4 cyfrowej liczby
+
 def get_random():
-    number = random.randint(1000, 9999)
-    return number
+    random_number = random.randint(1000, 9999)
+    return list(str(random_number))
 
-# Input od użytkownika
+
 def get_input():
-    # pętla z while / if która sprawdza input i nie pozwala przejść dalej dopóki
-    # nie zostanie podany 4 cyfrowy int
+    while True:
+        player_guess = input('Make your guess: ')
+        if len(player_guess) != 4:
+            print('This is not a correct value. 4 digits only')
+            continue
+            
+        else:
+            return list(player_guess)
+            break
 
-    # while True?
+def count_cows(random_number, player_guess):
+    cows = [digit for i, digit in enumerate(random_number) if digit == player_guess[i]]
+    random_number_left = [digit for i, digit in enumerate(random_number) if digit != player_guess[i]]
+    player_guess_left = [digit for i, digit in enumerate(player_guess) if digit != random_number[i]]
+    n_cows = len(cows)
+    return n_cows, random_number_left, player_guess_left
 
-    player_guess = int(input()) 
-    if len(player_guess) != 4:
-        print('alert')
-        continue
-    else:
-        break
-    
-# FUNKCJA:  Wylicz krowy
+def count_bulls(random_left, guess_left):
+    n_bulls = 0
+    for digit in guess_left:
+        if digit in random_left:
+            n_bulls += 1
+            random_left.remove(digit)
+    return n_bulls
 
-
-# FUNKCJA: Wylicz byki
-
-
-# FUNKCJA | Input od użytkownika i jego walidacja (czy jest liczbą)
 
 # FUNKCJA | krowy i byki
     # porównaj wylosowana i input
